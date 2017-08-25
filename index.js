815225f226d9e6c5c06092f9d203aa17deb56349
Mimi Chen <yangminchen91@gmail.com>
2017-08-25 00:31:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hello, World!")
+if (5>2) {
+  console.log("5 is greater than 2")
+}
+if (false) {
+  console.log("This will never get printed because the above statement evaluates to false.")
+} else {
+  console.log("This will get printed!")
+}
