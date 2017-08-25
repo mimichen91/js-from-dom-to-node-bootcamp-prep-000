98caaa2642352a232ed7d1b45d884479f873d508
Mimi Chen <yangminchen91@gmail.com>
2017-08-25 00:43:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ if (dog === "hungry") {
   console.log("Refilling food bowl.")
 } else if (dog === "thirsty") {
   console.log("Refilling water bowl.")
+} else if (dog === "playful") {
+  console.log("Playing ball.")
+} else if (dog === "cuddly") {
+  console.log("Snuggling.")
 } else {
   console.log("Reading newspaper.")
 }
