7c3d3911ef8530068cee33c79c7c20532ad761bb
Mimi Chen <yangminchen91@gmail.com>
2017-08-25 00:35:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,4 @@ if (6+4===9) {
 }
 if (false) {
   console.log("This will never get printed because the above statement evaluates to false.")
-} else {
-  console.log("This will get printed!")
-}
+} 
