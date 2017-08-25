4445b5fddf10389bd18c91eed0abbb330ee17527
Mimi Chen <yangminchen91@gmail.com>
2017-08-25 00:36:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-if (6+4===9) {
-  console.log("giraffes")
-}
-if (false) {
-  console.log("This will never get printed because the above statement evaluates to false.")
-} else {
-  console.log("This will get printed!")
+if (6 + 3 < 5) {
+  console.log("The hummingbird is the only animal that can fly backwards")
 }
